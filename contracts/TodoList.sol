e8e4450845d7524d0e097574d39850ac5d39bc3a
ikiliagwuC <ikiliagwuc@yahoo.com>
2022-03-13 15:26:19 +0100

    Project updated

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,26 @@ pragma solidity ^0.5.0;
 
 contract TodoList{
 	uint public taskCount = 0;
+
+	//model the tasks using structs
+	struct Task{
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	mapping(uint => Task) public tasks;
+
+	constructor() public{
+		createTask("check out Game theory documents");
+	}
+	
+
+	function createTask(string memory _content) public {
+		//increment task whenever it is called
+		taskCount++;
+		tasks[taskCount]= Task(taskCount, _content, false);
+	}
+
+
 }
